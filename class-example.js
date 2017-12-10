95833d53554b270b46dc0db863c2da0c0ca99e61
xiubinzheng <xiubin.zheng7@gmail.com>
2017-12-09 22:16:57 -0500

    latest

--- class-example.js
+++ class-example.js
@@ -7,7 +7,23 @@ Car.prototype.drive = function(){
     return 'vroom';
 }
 
-
+/*
 const car = new Car({title: 'Focus'});
+car.drive();
 car;
+*/
 
+var Toyota= function(options){
+    this.color  = options.color;
+}
+
+const toyota = new Toyota({color:'red', 
+title: 'Daily Driver'});
+
+Toyota.prototype = Object.create(Car.prototype);
+Toyota.prototype.constructor = Toyota;
+
+// lol, nobody understands this
+
+
+console.log(toyota);
