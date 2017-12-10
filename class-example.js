ae6e5d7da5edffd05348f51d7bb3e29fb44fd55e
xiubinzheng <xiubin.zheng7@gmail.com>
2017-12-09 22:25:16 -0500

    latest: before the classes

--- class-example.js
+++ class-example.js
@@ -14,16 +14,29 @@ car;
 */
 
 var Toyota= function(options){
+    Car.call(this,options);
     this.color  = options.color;
 }
-
+/*
 const toyota = new Toyota({color:'red', 
 title: 'Daily Driver'});
-
-Toyota.prototype = Object.create(Car.prototype);
-Toyota.prototype.constructor = Toyota;
+*/
 
 // lol, nobody understands this
 
+Toyota.prototype = Object.create(Car.prototype);
+Toyota.prototype.constructor = Toyota;
+Toyota.prototype.honk = function(){
+    return 'beep';
+}
+
+const toyota = new Toyota({color:'red',title:'daily Driver'});
+
+
+console.log('------');
+
+
 
 console.log(toyota);
+console.log(toyota.drive());
+console.log(toyota.honk());
